576fbba5538bc37b2ba7abd0d2e7de54d5f82bb3
tori engler <toriengler@0587353752.wireless.umich.net>
2018-01-30 14:56:43 -0500

    added check_questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,8 +1,13 @@
 def ask_question():
-    ask= input('What is your question? ')
+    ask=input ('What is your question? ')
+    while ask!= 'quit':
+        if ask[-1] !='?':
+            print("I’m sorry, I can only answer questions.")
+            ask=input ('What is your question? ')
 
-    questions=[]
-    save= questions.append(ask)
-    return save
+        questions=[]
+        save= questions.append(ask)
+        ask=input ('What is your question? ')
+
 
 ask_question()
